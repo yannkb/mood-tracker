3c69e9599524815c1bb76909095f70a3991b972c
Yann Bouchereau <yann.bchr@gmail.com>
2024-02-09 16:03:01 +1100

    📦 NEW: Add login and register

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import bootstrap from 'bootstrap';
 import htmx from 'htmx.org';
 
