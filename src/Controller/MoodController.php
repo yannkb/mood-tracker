0d5c00263f73b3f85abb0f8181e24f6db714fc5e
Yann Bouchereau <yann.bchr@gmail.com>
2024-02-16 19:42:23 +1100

    📦 NEW: redirect to mood index after successful login

--- src/Controller/MoodController.php
+++ src/Controller/MoodController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/mood', name: 'app_mood_')]
 class MoodController extends AbstractController
 {
-    #[Route('/mood', name: 'app_mood')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('mood/index.html.twig', [
